93ca1576d156d5026a86d15c5adfab4bb4cabd48
Tristan  Katic <tristankatic@Tristans-MBP.attlocal.net>
2021-08-05 11:37:58 -0500

    made some changes

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-console.log('Its working')
+console.log("testaroo");
 
-let theme = localStorage.getItem('theme')
+let theme = localStorage.getItem("theme");
 
-if(theme == null){
-	setTheme('light')
-}else{
-	setTheme(theme)
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
+let themeDots = document.getElementsByClassName("theme-dot");
 
-
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    console.log("Option clicked:", mode);
+    setTheme(mode);
+  });
 }
 
-function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
+function setTheme(mode) {
+  //change this later to be a light beige.
+  //make it more interesting and fun than white
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "css/default.css";
+  }
 
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
-	}
+  if (mode == "blue") {
+    //eh the blue is fine but it would be cooler to be a nice, dark grey
+    document.getElementById("theme-style").href = "css/blue.css";
+  }
 
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
+  if (mode == "green") {
+    //these are okay, but the order would be better as tan -> green -> purp -> dark grey
+    document.getElementById("theme-style").href = "css/green.css";
+  }
 
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
+  if (mode == "purple") {
+    //or tan -> green -> grey -> purp
+    document.getElementById("theme-style").href = "css/purple.css";
+  }
 
-	localStorage.setItem('theme', mode)
+  localStorage.setItem("theme", mode);
 }
