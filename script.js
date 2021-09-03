70a0578733cb364590eece289b7db032208280bd
Tristan  Katic <tristankatic@Tristans-MacBook-Pro.local>
2021-09-03 03:02:58 -0400

    rough draft of portfolio has been finished

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//console.log("testaroo");
+//console.log("testing123");
 
 let theme = localStorage.getItem("theme");
 
@@ -19,23 +19,19 @@ for (var i = 0; themeDots.length > i; i++) {
 }
 
 function setTheme(mode) {
-  //its okay to keep this as white and simple
   if (mode == "light") {
     document.getElementById("theme-style").href = "css/default.css";
   }
 
   if (mode == "blue") {
-    //eh the blue is fine but it would be cooler to be a nice, dark grey
     document.getElementById("theme-style").href = "css/blue.css";
   }
 
   if (mode == "green") {
-    //these are okay, but the order would be better as white -> green -> purp -> dark grey
     document.getElementById("theme-style").href = "css/green.css";
   }
 
   if (mode == "purple") {
-    //or tan -> green -> grey -> purp
     document.getElementById("theme-style").href = "css/purple.css";
   }
 
